10c3ce85a8e9c42b2846f6259fd80246d20f26f1
Alex Camargo <alexazevedo@Alexs-Mac-mini.local>
2024-08-05 22:03:01 -0300

    Adjust Points leaderboard

--- backend/index.js
+++ backend/index.js
@@ -10,32 +10,48 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/registerPresence', async (req, res) => {
-  const { adolescentId } = req.body;
-  const adolescent = await prisma.adolescent.update({
-    where: { id: adolescentId },
-    data: {
-      presences: { increment: 1 },
-      points: { increment: 50 },
-      presenceLogs: {
-        create: { date: new Date() }
-      }
-    },
-    include: { presenceLogs: true }
-  });
-  res.json(adolescent);
-});
-
-app.post('/registerCorrectAnswer', async (req, res) => {
-  const { adolescentId, correctAnswers } = req.body;
-  const adolescent = await prisma.adolescent.update({
-    where: { id: adolescentId },
-    data: {
-      correctAnswers: { increment: correctAnswers },
-      points: { increment: 60 * correctAnswers }
+    const { adolescentId, date } = req.body;
+    try {
+      const presenceDate = new Date(date);
+      const dayOfWeek = presenceDate.getUTCDay(); // 0 é domingo, 6 é sábado
+      const points = dayOfWeek === 0 ? 60 : 40;
+  
+      const adolescent = await prisma.adolescent.update({
+        where: { id: adolescentId },
+        data: {
+          presences: { increment: 1 },
+          points: { increment: points },
+          presenceLogs: {
+            create: { date: presenceDate }
+          }
+        },
+        include: { presenceLogs: true }
+      });
+      res.json(adolescent);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while registering presence.' });
     }
   });
-  res.json(adolescent);
-});
+  
+  app.post('/registerCorrectAnswer', async (req, res) => {
+    const { adolescentId, correctAnswers } = req.body;
+    try {
+      const points = correctAnswers * 10;
+  
+      const adolescent = await prisma.adolescent.update({
+        where: { id: adolescentId },
+        data: {
+          correctAnswers: { increment: correctAnswers },
+          points: { increment: points }
+        }
+      });
+      res.json(adolescent);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while registering correct answers.' });
+    }
+  });
 
 app.get('/leaderboard', async (req, res) => {
   const leaderboard = await prisma.adolescent.findMany({
